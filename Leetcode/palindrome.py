27596308e4c0cf9ea785903c0c33e64241e077e9
Shridhar Bhandar <shridhar2bhandar@gmail.com>
2025-05-31 12:38:47 +0530

    Create palindrome.py

---
+++ Leetcode/palindrome.py
@@ -0,0 +1,31 @@
+def isPalindrome(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0:
+        return False
+    if x % 10 == 0 and x != 0:
+        return False
+
+    original_num = x
+    iteration = 0
+    reverse_number = 0
+
+    while original_num > 0:
+        number = original_num % 10
+        reverse_number = reverse_number * 10 + number
+        original_num //= 10
+        iteration += 1
+    print(f"Reverse number is {reverse_number}")
+
+    if x == reverse_number:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    # Example usage
+    x = 123
+    result = isPalindrome(x)
+    print(result)
