744eb2347e3ae41088a73ecb8b982e25cda90194
Shridhar Bhandar <shridhar2bhandar@gmail.com>
2025-06-02 18:04:04 +0530

    Create intersection.py

---
+++ Leetcode/intersection.py
@@ -0,0 +1,25 @@
+def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # result = []
+        # for i in nums1:
+        #     for j in nums2:
+        #         if i == j:
+        #             result.append(i)
+        #
+        # result_set = set(result)
+        # return list(result_set)
+        ''' The Optimised code '''
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1 & set2)
+
+if __name__ == "__main__":
+    # Example usage
+    x = [1,2,2,1]
+    y = [2,2]
+    result = intersection(x, y)
+    print(result)
