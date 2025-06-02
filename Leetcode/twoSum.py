a530e37acf1014f0a399f8b9391aa4ac74a97551
Shridhar Bhandar <shridhar2bhandar@gmail.com>
2025-06-02 18:04:26 +0530

    Create twoSum.py

---
+++ Leetcode/twoSum.py
@@ -0,0 +1,16 @@
+def twoSum(self, nums, target):
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        print(f"Complement - {complement}, num - {num}")
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+        print(f"num_to_index - {num_to_index}")
+
+if __name__ == "__main__":
+    # Example usage
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = twoSum(nums, target)
+    print(result)
