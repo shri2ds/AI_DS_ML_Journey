5afff71b5db3ec61378821c8e47634147ff126f8
Shridhar Bhandar <shridhar2bhandar@gmail.com>
2025-11-29 01:13:42 +0530

    Refactor product_array_except_self and add usage example
    Added example usage for product_array_except_self function.

--- Leetcode/Product_Array_Except_Self.py
+++ Leetcode/Product_Array_Except_Self.py
@@ -1,16 +1,21 @@
-def product_array_except_self(self, nums):
-  """
-  :type nums: List[int]
-  """
-  output = [1] * len(nums)
-  # PASS 1: Build left products
-  prefix = 1
-  for i in range(len(nums)):
-      output[i] = prefix
-      prefix *= nums[i]
-  # PASS 2: Multiply by right products
-  suffix = 1
-  for i in range(len(nums)-1, -1, -1):
-      output[i] *= suffix
-      suffix *= nums[i]
-  return output
+def product_array_except_self(nums):
+    """
+    :type nums: List[int]
+    """
+    output = [1] * len(nums)
+    # PASS 1: Build left products
+    prefix = 1
+    for i in range(len(nums)):
+        output[i] = prefix
+        prefix *= nums[i]
+    # PASS 2: Multiply by right products
+    suffix = 1
+    for i in range(len(nums)-1, -1, -1):
+        output[i] *= suffix
+        suffix *= nums[i]
+    return output
+
+if __name__ == "main":
+    #Example usage
+    nums = [-2, -1, 0, 1, 2]
+    product_array_except_self(nums)
