05eaa1e51177c6a6c8b6a3f700ae470b0da45e82
Shridhar Bhandar <shridhar2bhandar@gmail.com>
2025-11-29 01:08:39 +0530

    Create Product_Array_Except_Self.py
    Prefix and Suffix implementation of Arrays

---
+++ Leetcode/Product_Array_Except_Self.py
@@ -0,0 +1,16 @@
+def product_array_except_self(self, nums):
+  """
+  :type nums: List[int]
+  """
+  output = [1] * len(nums)
+  # PASS 1: Build left products
+  prefix = 1
+  for i in range(len(nums)):
+      output[i] = prefix
+      prefix *= nums[i]
+  # PASS 2: Multiply by right products
+  suffix = 1
+  for i in range(len(nums)-1, -1, -1):
+      output[i] *= suffix
+      suffix *= nums[i]
+  return output
